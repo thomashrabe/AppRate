1a787dbdf254c8d9ef57fd2db100032f3252193f
Thomas Hrabe <thrabe@gmail.com>
2021-09-21 08:46:37 -0700

    android rate

--- android/src/main/java/com/bathymatrix/apprate/AppRate.java
+++ android/src/main/java/com/bathymatrix/apprate/AppRate.java
@@ -6,15 +6,41 @@ import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
 
+import com.google.android.play.core.review.ReviewInfo;
+import com.google.android.play.core.review.ReviewManager;
+import com.google.android.play.core.review.ReviewManagerFactory;
+import com.google.android.play.core.tasks.Task;
+
+
+
 @NativePlugin
 public class AppRate extends Plugin {
 
     @PluginMethod
     public void echo(PluginCall call) {
-        String value = call.getString("value");
+        Activity activity = this.cordova.getActivity();
+        ReviewManager manager = ReviewManagerFactory.create(activity);
+        Task<ReviewInfo> request = manager.requestReviewFlow();
 
-        JSObject ret = new JSObject();
-        ret.put("value", value);
-        call.success(ret);
+        request.addOnCompleteListener(task -> {
+
+            if (task.isSuccessful()) {
+                ReviewInfo reviewInfo = task.getResult();
+                Task<Void> flow = manager.launchReviewFlow(activity, reviewInfo);
+                flow.addOnCompleteListener(launchTask -> {
+                    if (task.isSuccessful()) {
+                        callbackContext.success();
+                    } else {
+                        Exception error = task.getException();
+                        callbackContext.error("Failed to launch review - " + error.getMessage());
+                    }
+                });
+            } else {
+                Exception error = task.getException();
+                callbackContext.error("Failed to launch review flow - " + error.getMessage());
+            }
+
+        });
+        call.success();
     }
 }
